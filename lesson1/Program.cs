2321ccef84f8b10ce23a632ebdaec4f0f56d9517
SofiaPodugorova <86651860+SofiaPodugorova@users.noreply.github.com>
2021-06-29 20:29:17 +0300

    Lesson 1

---
+++ lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            int age = 14;
+            DateTime dateTime = DateTime.Now;
+            string date = DateTime.Now.ToString();
+            Console.WriteLine($"Hello, my name is {name}, age = {age} , time is {dateTime}");
+            Console.ReadKey();
+        }
+    }
+}
