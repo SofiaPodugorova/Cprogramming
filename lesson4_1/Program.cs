171e80727abfb9b5e54fb2fa8c6707310697ae83
SofiaPodugorova <86651860+SofiaPodugorova@users.noreply.github.com>
2021-07-01 13:22:09 +0300

    дз3

---
+++ lesson4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = {
+                { "Елена", "+38058417895" }, 
+                { "Даша", "+380564211445" },
+                { "Виталина", "+380585462445" }, 
+                { "Дмитрий", "+380556891445" }, 
+                { "Иван", "+380575861445" }};
+            Console.WriteLine("Телефонный спрвочник : ");
+            for (int i = 0;i< array.GetLength(0);i++)
+            {
+                Console.WriteLine($"{array[i, 0]},{array[i, 1]}");
+            }
+        }
+    }
+}
---
+++ lesson4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //обратное слово
+            string line = Console.ReadLine();
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(line[i]);
+            }
+        }
+    }
+}
