b4025246b23cd32fa9eaa1dc177b04f006a0bcee
SofiaPodugorova <86651860+SofiaPodugorova@users.noreply.github.com>
2021-07-06 14:05:04 +0300

    дз3 урок

---
+++ lesson5_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace lesson5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "bytes.bin";
+            while (true)
+            {
+                Console.WriteLine("Введите числа от 0 до 225");
+                var line = Console.ReadLine();
+                byte[] array = GetByteArray(line, out bool correctLine);
+                if (correctLine)
+                {
+                    File.WriteAllBytes(fileName, array);
+                    break;
+                }
+           
+            }
+            Console.WriteLine($"Запись в файл {fileName}");
+            Console.ReadKey();
+        }
+
+        private static byte[] GetByteArray(string line, out bool correctLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
