99ea5fa726e92d27cf9e849cd674415cb00be62a
SofiaPodugorova <86651860+SofiaPodugorova@users.noreply.github.com>
2021-07-06 14:05:24 +0300

    урок 5

---
+++ lesson5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя файла");
+            var fileName = GenerateFileName();
+            Console.WriteLine("Введите данные");
+        }
+
+        private static object GenerateFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
