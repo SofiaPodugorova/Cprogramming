382264f86d9a0b82782b17652788fa3f51c85b61
SofiaPodugorova <86651860+SofiaPodugorova@users.noreply.github.com>
2022-04-03 17:26:44 +0300

    разложение на простые множители

---
+++ ProstMnoj/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstMnoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            int b, c, n;
+
+            Console.WriteLine("Введите номер ");
+            if (!int.TryParse(Console.ReadLine(), out n)) return;
+
+            while ((n % 2) == 0)
+            {
+                n = n / 2;
+                s += "2*";
+            }
+            b = 3; c = (int)Math.Sqrt(n) + 1;
+            while (b < c)
+            {
+                if ((n % b) == 0)
+                {
+                    if (n / b * b - n == 0)
+                    {
+                        s += b.ToString() + "*";
+                        n = n / b;
+                        c = (int)Math.Sqrt(n) + 1;
+                    }
+                    else
+                        b += 2;
+                }
+                else
+                    b += 2;
+            }
+            s += n.ToString();
+            Console.WriteLine(s);
+            Console.ReadKey(true);
+        }
+    }
+}
